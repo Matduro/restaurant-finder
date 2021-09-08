e12318fc93971daf936109a131adcd290ae87b66
Mathieu <mathieurousseau123@gmail.com>
2021-09-08 16:39:03 -0700

    Develop routes for app

--- server/db/db.sql
+++ server/db/db.sql
@@ -7,4 +7,24 @@ CREATE TABLE restaurants (
 
 INSERT INTO restaurants (name, location, price_range) values ('mcdonalds', 'new yorks', 3);
 INSERT INTO restaurants (name, location, price_range) values ('pizza hut', 'vegas', 2);
-INSERT INTO restaurants (name, location, price_range) values ('we`ndys', 'denver', 3);
+INSERT INTO restaurants (name, location, price_range) values ('wendys', 'denver', 3);
+
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(
+        rating >= 1
+        and rating <= 5
+    )
+);
+select *
+from restaurants
+    left join(
+        select restaurant_id,
+            count(*),
+            TRUNC(AVG(rating, 1)) as average_rating
+        from reviews
+        group by restaurant_id
+    ) reviews on restaurants.id = reviews.restaurant_id;
